46e1a9d1f53244ed8559069486af2ea9a752a86b
seifo <mettaiseifedin@gmail.com>
2024-10-02 14:39:25 -0700

    ousssama hi

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -53,7 +53,7 @@ if not IS_HEROKU_APP:
 if IS_HEROKU_APP:
     ALLOWED_HOSTS = ["*"]
 else:
-    ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "0.0.0.0"]
+    ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "0.0.0.0", "django-app-using-cicd.onrender.com"]
 
 
 # Application definition
